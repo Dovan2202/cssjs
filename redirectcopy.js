c0592e3c8cc8e68cc4367cea89ddc3ac07c4d783
Dovan2202 <84137534+Dovan2202@users.noreply.github.com>
2021-05-15 18:25:09 +0700

    Add files via upload

---
+++ redirectcopy.js
@@ -0,0 +1,16 @@
+mfunction FSAddOriginalLink(){
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();https://historemobile.blogspot.com/'>https://historemobile.blogspot.com/"+document.location.pathname+"</a>";
+    var copytext = pagelink;
+    var newdiv = document.createElement('div');
+    newdiv.style.position='absolute';
+    newdiv.style.left='-99999px';
+    body_element.appendChild(newdiv);
+    newdiv.innerHTML = copytext;
+    selection.selectAllChildren(newdiv);
+    window.setTimeout(function() {
+        body_element.removeChild(newdiv);
+    },0);
+}
+document.oncopy = FSAddOriginalLink;
